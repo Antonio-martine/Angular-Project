7d122c1948fef387e642f875a0c929d6df0a5cd6
Antonio-martine <jesus07antonio03@gmail.com>
2023-04-12 14:32:30 -0600

    Proyecto Final Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment.prod';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { VerificarEmailComponent } from './pages/verificar-email/verificar-email.component';
+import { ServicesAuthService } from './services/services-auth.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    ForgotPasswordComponent,
+    VerificarEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    ServicesAuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
